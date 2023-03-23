d96400d1059b8773d5d5a80d3e8e2dde70b4c036
Angulorecto <120491110+Angulorecto@users.noreply.github.com>
2023-03-23 10:54:44 -0400

    Update cool.js

--- cool.js
+++ cool.js
@@ -2,7 +2,8 @@ const name = prompt("What text would you like to transalate?");
 var text = "";
 function alfabetCheck() {
   if name.includes("E") {
-    text.value += "𝕰"
-    alert(text)
+    text.value += "𝕰";
+    alert(text);
   }
 }
+alfabetCheck();
