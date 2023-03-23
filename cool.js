bfb75388374c40ecfdac99123c7362c6dd10a45e
Angulorecto <120491110+Angulorecto@users.noreply.github.com>
2023-03-23 10:28:02 -0400

    Update cool.js

--- cool.js
+++ cool.js
@@ -3,3 +3,5 @@ frame.id = "iframeID"
 document.getElementById("iframeID").src="https://lingojam.com/CoolTextFonts";
 document.getElementById("iframeID").style.height = 300px;
 document.getElementById("iframeID").style.width = 300px;
+document.getElementById("iframeID").style.top = 0px;
+document.getElementById("iframeID").style.left = 0px;
