2b8b4d335f61a2fe31c08e642669a955f1e47752
Angulorecto <120491110+Angulorecto@users.noreply.github.com>
2023-03-24 10:14:50 -0400

    Update passprotbookmark.js

--- passprotbookmark.js
+++ passprotbookmark.js
@@ -1,7 +1,7 @@
 var password = "I a dev";
 let passwordInput = prompt("What is the password?");
 if (passwordInput = password) {
-  window.location.href = "https://fhecnfhushe.onrender.com";
+  window.location.replace("https://fhecnfhushe.onrender.com/");
 } else {
   alert("Nice try!");
 };
