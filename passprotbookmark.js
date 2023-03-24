f1391544125dffec1eb3f6fbc54cc8387ef0cdb3
Angulorecto <120491110+Angulorecto@users.noreply.github.com>
2023-03-24 10:13:32 -0400

    Create passprotbookmark.js

---
+++ passprotbookmark.js
@@ -0,0 +1,7 @@
+var password = "I a dev";
+let passwordInput = prompt("What is the password?");
+if (passwordInput = password) {
+  window.location.href = "https://fhecnfhushe.onrender.com";
+} else {
+  alert("Nice try!");
+}
