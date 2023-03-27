0af55a827140973edcc071907f035a6d33c037f7
Angulorecto <120491110+Angulorecto@users.noreply.github.com>
2023-03-27 11:40:57 -0400

    Update passwordProtMaker.js

--- passwordProtMaker.js
+++ passwordProtMaker.js
@@ -1,3 +1,7 @@
+function copy(stuff) {
+  navigator.clipboard.writeText(stuff);
+}
 let password = prompt("What's gonna be the password?");
 let url = prompt("What's the website url?");
-let response = prompt("Copy this and bookmark it:","javascript:let passwordInput = prompt(" + '"What is the password?"' + ");if (passwordInput == " + '""' + ") {  alert("+'"Nice try!"' + ");} else if (passwordInput != " + '"' + password + '"' + ") {  alert(" + '"Nice try!"'+");} else {  window.location.replace("+ '"' + url + '"' +");};");
+copy("javascript:let passwordInput = prompt(" + '"What is the password?"' + ");if (passwordInput == " + '""' + ") {  alert("+'"Nice try!"' + ");} else if (passwordInput != " + '"' + password + '"' + ") {  alert(" + '"Nice try!"'+");} else {  window.location.replace("+ '"' + url + '"' +");};");
+let response = alert("Copied!");
