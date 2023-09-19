7f125575bb9aae739e1e5c0f96b9ffe5215aaa32
AnguloRecto <120491110+Angulorecto@users.noreply.github.com>
2023-09-19 10:15:59 -0400

    Update E.js

--- E.js
+++ E.js
@@ -1,8 +1,19 @@
 var name = document.getElementById("organization-profile-name");
 var email = document.getElementById("organization-billing-email");
 //Words
-const OrgAsNetCom = ["","","","","","","","","",];
-//descition
-const randomCorp = Math.floor(Math.random() * OrgAsNetCom.length);
-var corp = 
-name.value = random;
+const randomParent = Math.floor(Math.random() * 1);
+var parent = "default";
+if (randomParent == 0) {
+    parent = "Network";
+} else if (randomParent == 1) {
+    parent = "Organization";
+} else {
+    parent = "Association";
+};
+
+const Adjs = ["","","","","","","","","","",""];
+const Noun = ["","","","","","","","","","",""];
+
+const randomAdj = Math.floor(Math.random() * Adjs.length);
+var corp = Adjs[randomAdj];
+name.value = Adj;
