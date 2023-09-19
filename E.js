c67ca3e3b3898a3a00bb10ddc57919bd9fb18996
AnguloRecto <120491110+Angulorecto@users.noreply.github.com>
2023-09-19 09:15:44 -0400

    Update E.js

--- E.js
+++ E.js
@@ -1,1 +1,8 @@
 var name = document.getElementById("organization-profile-name");
+var email = document.getElementById("organization-billing-email");
+//Words
+const OrgAsNetCom = ["","","","","","","","","",];
+//descition
+const randomCorp = Math.floor(Math.random() * OrgAsNetCom.length);
+var corp = 
+name.value = random;
